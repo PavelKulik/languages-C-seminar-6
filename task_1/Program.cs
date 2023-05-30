eb415861a2faacec610d4e9ca8c420b47f8202c1
Pavel Kulik <Kanstantin0105@gmail.com>
2023-05-30 22:31:29 +0300

    task_1 add

---
+++ task_1/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] GetArray(int size)
+        {
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Введите {i + 1} элемент: ");
+                array[i] = int.Parse(Console.ReadLine()!);
+            }
+
+            return array;
+        }
+
+        void PrintArray(int[] array)
+        {
+            Console.Write($"[{String.Join(", ", array)}]");
+        }
+
+        int CountPositiveNumbers(int[] array)
+        {
+            int count = 0;
+            foreach (int el in array)
+            {
+                if (el > 0)
+                    count++;
+            }
+
+            return count;
+        }
+
+        Console.Write("Введите количество элементов массива: ");
+        int size = int.Parse(Console.ReadLine()!);
+        int[] array = GetArray(size);
+        int count = CountPositiveNumbers(array);
+        PrintArray(array);
+        Console.WriteLine($" -> {count}");
+    }
+}
